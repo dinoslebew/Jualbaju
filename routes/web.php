24af8fcf83195fc0ffb827b070854083b000d727
dinoslebew <awdino966@gmail.com>
2025-03-12 14:47:07 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\listbarangController;
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'Users dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Dashboard Admin';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin User';
+    });
+});
+
+//Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+//   return view('list_barang', compact('id', 'nama'));
+//});
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
